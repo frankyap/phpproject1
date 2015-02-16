e6a64c7bef08708fef98e4c1b564e434f8339086
francisco <franciscoyap@yahoo.com>
2015-02-15 21:04:03 -0800

    yesyesyes

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-         echo "yes yes yes"; //e put your code here
+         echo "yes yes yes yes"; //e put your code here
         ?>
     </body>
 </html>
